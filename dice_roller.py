f1aa2336bc1a0cd736dff9d37baef0824f84de78
Qin Yi Qi <applesweet-e@hotmail.com>
2021-01-18 20:52:25 +0800

    now rolling 3 dices

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,18 @@
-def main():
-  #print('You rolled a die')
+import random
 
-  if __name__== "__main__":
-    main()
+def main():
+
+  dice_sum = 0
+  dice_rolls = 3
+
+  for i in range(0, dice_rolls):
+    roll = random.randint(1,6)
+    dice_sum += roll
+    print(f'You rolled a {roll}')
+
+  print(f'You have rolled a total of {dice_sum}')
+  
+  
+
+if __name__== "__main__":
+  main()
